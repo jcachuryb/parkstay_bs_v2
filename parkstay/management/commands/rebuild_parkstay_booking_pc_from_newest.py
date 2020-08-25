31f9cb838c465d3d958081d191dae29c8131de9d
Jason Moore <jason.moore@dbca.wa.gov.au>
2020-08-25 16:19:11 +0800

    Rebuild Property Cache Threaded

--- parkstay/management/commands/rebuild_parkstay_booking_pc_from_newest.py
+++ parkstay/management/commands/rebuild_parkstay_booking_pc_from_newest.py
@@ -1,31 +1,61 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.conf import settings
+import threading
 
 #from mooring.models import RegisteredVessels
 from parkstay import models
 from datetime import datetime
 import json
-
+import time
 from datetime import timedelta
-
+globalcount = 0
 class Command(BaseCommand):
     help = 'Rebuild mooring booking property cache.'
 
     def handle(self, *args, **options):
-
+        print ("current version" + settings.BOOKING_PROPERTY_CACHE_VERSION)
         try:
-           bookings = models.Booking.objects.all().order_by('-id')
+           bookings = models.Booking.objects.exclude(property_cache__cache_version=settings.BOOKING_PROPERTY_CACHE_VERSION).order_by('-id')
+           print (bookings)
+           globalcount = 0
            for b in bookings:
+               globalcount = globalcount + 1
+               t = None
                try: 
-                   b.property_cache['cache_version'] 
+                   b.property_cache['cache_version']
+                   print (b.property_cache['cache_version'])
+                   if b.property_cache['cache_version'] != settings.BOOKING_PROPERTY_CACHE_VERSION:
+                       print ("Rebuilding :"+str(b.id))
+                       t = threading.Thread(target=update_cache,args=[b.id,],daemon=False)
+ 
                except:
-                    b.update_property_cache()
-               if b.property_cache['cache_version'] != settings.BOOKING_PROPERTY_CACHE_VERSION:
-                    print ("Rebuilding :"+str(b.id))
-                    b.update_property_cache()
+                    #b.update_property_cache()
+                    t = threading.Thread(target=update_cache,args=[b.id,],daemon=False)
+
+               #if b.property_cache['cache_version'] != settings.BOOKING_PROPERTY_CACHE_VERSION:
+               #     print ("Rebuilding :"+str(b.id))
+               #     t = threading.Thread(target=update_cache,args=[b.id,],daemon=True)
+               #print ('thread')
+               #print (t)
+               if t is not None:
+                    if globalcount > 9:
+                         print ("Sleeping")      
+                         time.sleep(10)
+                         globalcount = 0
+                    t.start()
+
+                    #b.update_property_cache()
 
         except Exception as e:
             print (e)
             #Send fail email
             content = "Error message: {}".format(e)
+
+def update_cache(booking_id):
+     print ('New Rebuild for: '+str(booking_id))
+     b= models.Booking.objects.get(id=int(booking_id))
+     b.update_property_cache(False)
+     print ('Finished :'+str(booking_id))
+
+
