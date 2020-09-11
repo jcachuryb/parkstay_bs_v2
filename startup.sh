fc4cb074c8c858e9ce35c50df1f929868ad33e40
Jason Moore <jason.moore@dbca.wa.gov.au>
2020-09-11 11:36:44 +0800

    Docker config changes

--- startup.sh
+++ startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-  
+
 # Start the first process
 env > /etc/.cronenv
 sed -i 's/\"/\\"/g' /etc/.cronenv
 
+if [ $ENABLE_CRON == "True" ];
+then
+echo "Starting Cron"
 service cron start &
 status=$?
 if [ $status -ne 0 ]; then
@@ -11,6 +14,11 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
+fi
+
+if [ $ENABLE_WEB == "True" ];
+    then
+echo "Starting Gunicorn"
 # Start the second process
 gunicorn parkstay.wsgi --bind :8080 --config /app/gunicorn.ini
 status=$?
@@ -18,4 +26,7 @@ if [ $status -ne 0 ]; then
   echo "Failed to start gunicorn: $status"
   exit $status
 fi
-
+else
+   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   /bin/bash
+fi
