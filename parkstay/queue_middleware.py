3d24c252b5f48e098b1543dd5121e38a64c9280e
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-11-04 21:23:15 +0800

    Queue Middleware

---
+++ parkstay/queue_middleware.py
@@ -0,0 +1,43 @@
+import re
+import datetime
+import requests
+from django.conf import settings
+#from django.core.urlresolvers import reverse
+from django.urls import reverse
+from django.http import Http404, HttpResponse, JsonResponse, HttpResponseRedirect
+from django.utils import timezone
+
+CHECKOUT_PATH = re.compile('^/ledger-api')
+PROCESS_PAYMENT =  re.compile('^/ledger-api/process-payment')
+
+class QueueControl(object):
+
+    def __init__(self, get_response):
+            self.get_response = get_response
+
+    def __call__(self, request):
+       response= self.get_response(request)
+       sitequeuesession = request.COOKIES.get('sitequeuesession', None)
+       if request.path == '/' or request.path.startswith('/search-availability/information/') or request.path.startswith('/search-availability/campground'):
+
+            try:
+                 if 'HTTP_HOST' in request.META:
+                      if settings.QUEUE_ACTIVE_HOSTS == request.META.get('HTTP_HOST',''):
+                           print (settings.QUEUE_WAITING_URL)
+                           if settings.QUEUE_WAITING_URL:
+                               url = settings.QUEUE_URL+"/api/check-create-session/?session_key="+sitequeuesession+"&queue_group="+settings.QUEUE_GROUP_NAME
+                               resp = requests.get(url, data = {}, cookies={},  verify=False)
+                               queue_json = resp.json()
+                               if queue_json['status'] == 'Waiting': 
+                                     response =HttpResponse("<script>window.location.replace('"+settings.QUEUE_WAITING_URL+"');</script>Redirecting")
+                                     return response
+                                     print ('You are waiting')
+                               else:
+                                     print ('Active Session')
+            except Exception as e:
+                print (e)
+                print ("ERROR LOADING QUEUE")
+       else:
+            pass
+
+       return response
