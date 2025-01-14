0a40dbdb4f1d1b1da5f61afa8efeea574c9bd7ea
Juan Camilo Achury Bueno <jcachuryb@outlook.com>
2025-01-14 15:02:12 +0800

    Included Legacy scripts for older browser versions

--- parkstay/frontend/parkstay/vite.config.js
+++ parkstay/frontend/parkstay/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
-import inject from "@rollup/plugin-inject";
-import commonjs from '@rollup/plugin-commonjs';
-import { babel } from '@rollup/plugin-babel';
+import legacy from "@vitejs/plugin-legacy";
 
 // import resolve from '@rollup/plugin-node-resolve';
 import path from "path";
@@ -20,7 +18,6 @@ export default defineConfig(({ mode }) => {
 
   const config = conf[environment];
   const env = config.env;
-  console.log(config);
   return {
     base: ``,
     define: {
@@ -28,60 +25,64 @@ export default defineConfig(({ mode }) => {
       "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
     },
     server: {
-        host: host,
+      host: host,
+      port: port,
+      strictPort: true,
+      open: false,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
+      hmr: {
+        protocol: "ws",
+        host: "localhost",
         port: port,
-        strictPort: true,
-        open: false,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers':
-                'Origin, X-Requested-With, Content-Type, Accept',
-        },
-        hmr: {
-            protocol: 'ws',
-            host: "localhost",
-            port: port,
-        },
+      },
     },
     plugins: [
-        vue(),
-        // eslint(),
-        svgLoader({
-            defaultImport: 'url',
-        }),
+      vueDevTools(),
+      vue(),
+      // eslint(),
+      svgLoader({
+        defaultImport: "url",
+      }),
+      legacy({
+        targets: ["defaults", "> 1%", "last 2 versions", "not ie <= 8"],
+      }),
     ],
     resolve: {
-        alias: {
-            vue: "vue/dist/vue.esm-bundler.js",
-            '@': path.resolve(__dirname, './src'),
-            '@vue-utils': path.resolve(__dirname, 'src/utils/vue'),
-            '@common-utils': path.resolve(__dirname, 'src/components/common/'),
-            '@select2': 'select2/dist/js/select2.min.js',
-        },
+      alias: {
+        vue: "vue/dist/vue.esm-bundler.js",
+        "@": path.resolve(__dirname, "./src"),
+        "@vue-utils": path.resolve(__dirname, "src/utils/vue"),
+        "@common-utils": path.resolve(__dirname, "src/components/common/"),
+        "@select2": "select2/dist/js/select2.min.js",
+      },
     },
     build: {
-        manifest: 'manifest.json',
-        filenameHashing: false,
-        commonjsOptions: { transformMixedEsModules: true },
-        root: path.resolve(__dirname, './src'),
-        outDir: path.resolve(
-            __dirname,
-            `../../static/${applicationNameShort}_vue`
-        ),
-        publicPath: ``,
-        sourcemap: true,
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'src/apps/main.js'),
-            },
-            output: {
-                entryFileNames: 'js/[name].js',
-                chunkFileNames: 'js/[name].js',
-                assetFileNames: '[ext]/[name].[ext]',
-            },
-            external: ["jquery", "bootstrap", "datatables"],
+      manifest: "manifest.json",
+      filenameHashing: false,
+      commonjsOptions: { transformMixedEsModules: true },
+      root: path.resolve(__dirname, "./src"),
+      outDir: path.resolve(
+        __dirname,
+        `../../static/${applicationNameShort}_vue`
+      ),
+      publicPath: ``,
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "src/apps/main.js"),
         },
-        emptyOutDir: true,
+        output: {
+          entryFileNames: "js/[name].js",
+          chunkFileNames: "js/[name].js",
+          assetFileNames: "[ext]/[name].[ext]",
+        },
+        external: ["jquery", "bootstrap", "datatables"],
+      },
+      emptyOutDir: true,
     },
-  }
+  };
 });
